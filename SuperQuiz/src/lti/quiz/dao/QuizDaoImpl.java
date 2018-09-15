c07000ae9bbfb36db926efa24fce509d6c0da79e
PC1 <PC1@Infva06203>
2018-09-15 15:18:13 +0530

    second

--- SuperQuiz/src/lti/quiz/dao/QuizDaoImpl.java
+++ SuperQuiz/src/lti/quiz/dao/QuizDaoImpl.java
@@ -1,22 +1,30 @@
 package lti.quiz.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import lti.quiz.bean.OptionBean;
 import lti.quiz.bean.QuizBean;
-import oracle.jdbc.OracleDriver;
 
 public class QuizDaoImpl implements QuizDao {
 	private Connection getConnection() throws SQLException {
-		DriverManager.registerDriver(new OracleDriver());
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		Connection conn = DriverManager.getConnection(url, "zombie", "oracle");
-		return conn;
+		try {
+			Context initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource)envContext.lookup("jdbc/quiz");
+			Connection conn = ds.getConnection();
+			return conn;
+		} catch (NamingException e) {
+			throw new SQLException(e.getMessage());
+		}
 	}
 
 	@Override
--- SuperQuiz/src/lti/quiz/dao/UserDaoImpl.java
+++ SuperQuiz/src/lti/quiz/dao/UserDaoImpl.java
@@ -1,23 +1,31 @@
 package lti.quiz.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import lti.quiz.bean.ForgetBean;
 import lti.quiz.bean.LoginBean;
 import lti.quiz.bean.RegisterBean;
-import oracle.jdbc.OracleDriver;
 
 public class UserDaoImpl implements UserDao {
 	
 	private Connection getConnection() throws SQLException {
-		DriverManager.registerDriver(new OracleDriver());
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		Connection conn = DriverManager.getConnection(url, "zombie", "oracle");
-		return conn;
+		try {
+			Context initContext = new InitialContext();
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource)envContext.lookup("jdbc/quiz");
+			Connection conn = ds.getConnection();
+			return conn;
+		} catch (NamingException e) {
+			throw new SQLException(e.getMessage());
+		}
 	}
 
 	@Override
